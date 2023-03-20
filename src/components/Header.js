82cfe19d160f8200c37715a94ab2fce989390df5
AlexBelic <sajko.belic@gmail.com>
2023-03-20 10:50:47 +0100

    Portfolio
    Portfolio web page create in React

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const Header = () => {
-  return <div>Header</div>;
+  return <header className='py-8'>
+    <div className='container mx-auto'>
+      <div className='flex justify-between items-center'>
+        {/* logo */}
+        <a href='#'>          
+          <h1 className='text-3xl font-bold'>ALEXANDAR BELIĆ</h1>
+        </a>
+        {/* button */}
+        <button className='btn btn-sm'>Work with me</button>
+      </div>      
+    </div>
+  </header>;
 };
 
 export default Header;
